7babbd99dde84c690556ca3d14a29b15408d03ef
Jitendra Singh Nimod <i.jitendra.singh0@gmail.com>
2026-03-18 20:02:35 +0530

    Multi-file update via Advanced Repo Manager

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // svelte.config.js
 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
